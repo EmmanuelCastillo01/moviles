e968c18e43f7718a0361da3ff4426dfd4cca9b1a
EmmanuelCastillo01 <davidemmanuelbc@gmail.com>
2025-05-23 01:02:03 -0600

    vizualisar recetas funciona

--- app/src/main/java/com/example/psm_pia/MainActivity.kt
+++ app/src/main/java/com/example/psm_pia/MainActivity.kt
@@ -30,7 +30,7 @@ import java.net.URLEncoder
 import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-
+//fd
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
